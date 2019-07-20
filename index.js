f25fbc84f9d660e3cecdaae09a5f20501c3931b6
Timoharj <timoharj@metropolia.fi>
2019-07-20 17:27:17 +0300

    added default variable to person variable in index.js

--- index.js
+++ index.js
@@ -5,23 +5,30 @@ const morgan = require('morgan')
 
 
 const cors = require('cors')
-const persons = [
-     {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
     },
     {
-      "name": "Olavi Virta",
-      "number": "12-55-2222131",
-      "id": "Olavi Virta"
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 4
     },
     {
-      "name": "Teuvo Hakkarainen",
-      "number": "013-155-1233-111",
-      "id": "Teuvo Hakkarainen"
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
-  ];
+
+]
 
 
 app.use(express.static('build'))
