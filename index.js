33252044c5a2d8d00593612c9cb593f3a38b5eab
Timoharj <timoharj@metropolia.fi>
2019-07-20 06:26:14 +0300

    build ui

--- index.js
+++ index.js
@@ -3,34 +3,13 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-app.use(express.static('build'))
 
 const cors = require('cors')
+
+app.use(express.static('build'))
+
 app.use(cors())
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 4
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-
-]
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
